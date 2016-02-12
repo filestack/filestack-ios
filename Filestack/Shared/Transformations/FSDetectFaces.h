45dde127b4099c01760cf7fe78f73ce283d2f42c
pJes2 <lukasz@filestack.com>
2016-02-12 14:39:19 +0100

    Add comment docs for FSBlurFaces, FSCropFaces, FSDetectFaces and FSPixelateFaces

--- Filestack/Shared/Transformations/FSDetectFaces.h
+++ Filestack/Shared/Transformations/FSDetectFaces.h
@@ -10,9 +10,21 @@
 
 @interface FSDetectFaces : FSTransform
 
+/**
+ This parameter is used to weed out objects that most likely are not faces. This can be an integer or a float in a range from 0.01 to 10000.
+ */
 @property (nonatomic, strong) NSNumber *minSize;
+/**
+ This parameter is used to weed out objects that most likely are not faces. This can be an integer or a float in a range from 0.01 to 10000.
+ */
 @property (nonatomic, strong) NSNumber *maxSize;
+/**
+ Change the color of the face object boxes and text. This can be the word for a color, or the hex color code.
+ */
 @property (nonatomic, strong) NSString *color;
+/**
+ If `YES`, it will export all face objects to a JSON object.
+ */
 @property (nonatomic, assign) BOOL exportToJSON;
 
 - (instancetype)initWithMinSize:(NSNumber *)minSize maxSize:(NSNumber *)maxSize color:(NSString *)color exportToJSON:(BOOL)exportToJSON;
