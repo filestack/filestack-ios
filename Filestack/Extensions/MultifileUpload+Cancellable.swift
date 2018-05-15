b9312b95ad0b059e0813c5e096ae3e85fbc9c77f
Mihály Papp <michal@papp.pl>
2018-05-15 17:43:33 +0200

    Added cancellable extension to MultifileUpload

---
+++ Filestack/Extensions/MultifileUpload+Cancellable.swift
@@ -0,0 +1,15 @@
+//
+//  MultifileUpload+Cancellable.swift
+//  Filestack
+//
+//  Created by Mihály Papp on 15/05/2018.
+//  Copyright © 2018 Filestack. All rights reserved.
+//
+
+import Foundation
+import FilestackSDK
+
+
+extension MultifileUpload: CancellableRequest {
+    
+}
