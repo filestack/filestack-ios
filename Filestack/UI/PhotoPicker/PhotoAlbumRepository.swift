ee83f306b106e11273021fe7e0d009016b7379b4
Mihály Papp <michal@papp.pl>
2018-06-01 08:37:02 +0200

    Repository for fetching assets

---
+++ Filestack/UI/PhotoPicker/PhotoAlbumRepository.swift
@@ -0,0 +1,42 @@
+//
+//  PhotoAlbumRepository.swift
+//  Filestack
+//
+//  Created by Mihály Papp on 29/05/2018.
+//  Copyright © 2018 Filestack. All rights reserved.
+//
+
+import Photos
+
+struct Album {
+  let title: String
+  let elements: [PHAsset]
+}
+
+class PhotoAlbumRepository {
+  
+  private var cachedAlbums: [Album]?
+  
+  init() {
+    fetchAndCacheAlbums(completion: nil)
+  }
+  
+  func getAlbums(completion: @escaping ([Album]) -> Void) {
+    if let cachedAlbums = cachedAlbums {
+      completion(cachedAlbums)
+    }
+    fetchAndCacheAlbums(completion: completion)
+  }
+  
+  private func fetchAndCacheAlbums(completion: (([Album]) -> Void)?) {
+    DispatchQueue.global(qos: .default).async {
+      let collections = PHAssetCollection.allCollections(types: [.album, .smartAlbum])
+      let allAlbums = collections.map { Album(title: $0.localizedTitle ?? "", elements: $0.allAssets) }
+      let nonEmptyAlbums = allAlbums.filter { $0.elements.count > 0 }
+      self.cachedAlbums = nonEmptyAlbums
+      completion?(nonEmptyAlbums)
+    }
+
+  }
+}
+
