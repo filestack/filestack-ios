edf431b1eaa3acd57524015c12514cb92796a4ff
Ruben Nine <ruben@9labs.io>
2019-07-26 17:51:40 +0200

    Minor refactoring of demo project.

--- Demo/FilestackDemo/AppDelegate.swift
+++ Demo/FilestackDemo/AppDelegate.swift
@@ -6,15 +6,29 @@
 //  Copyright © 2017 Filestack. All rights reserved.
 //
 
+import AVFoundation.AVAssetExportSession
 import Filestack
+import FilestackSDK
 import UIKit
 
-// Make sure the app URL scheme matches that one set in your info plist, in this demo this is "filestackdemo".
+// Set your app's URL scheme here.
 let appURLScheme = "filestackdemo"
+// Set your Filestack's API key here.
+let filestackAPIKey = "YOUR-FILESTACK-API-KEY"
+// Set your Filestack's app secret here.
+let filestackAppSecret = "YOUR-FILESTACK-APP-SECRET"
+// Filestack Client, nullable
+var client: Filestack.Client?
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupFilestackClient()
+
+        return true
+    }
 
     func application(_: UIApplication, open url: URL, options _: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         if url.scheme == appURLScheme, url.host == "Filestack" {
@@ -22,5 +36,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
 
         return false
+    }
+
+    private func setupFilestackClient() {
+        // In case your Filestack account has security enabled, you will need to instantiate a `Security` object.
+        // We can do this by either configuring a `Policy` and instantiating a `Security` object by passing
+        // the `Policy` and an `appSecret`, or by instantiating a `Security` object directly by passing an already
+        // encoded policy together with its corresponding signature — in this example, we will use the 1st method.
+
+        // Create `Policy` object with an expiry time and call permissions.
+        let policy = Policy(expiry: .distantFuture,
+                            call: [.pick, .read, .stat, .write, .writeURL, .store, .convert, .remove, .exif])
+
+        // Create `Security` object based on our previously created `Policy` object and app secret obtained from
+        // [Filestack Developer Portal](https://dev.filestack.com/).
+        guard let security = try? Security(policy: policy, appSecret: filestackAppSecret) else {
+            fatalError("Unable to instantiate Security object.")
+        }
+
+        // Create `Config` object.
+        let config = Filestack.Config.builder
+            // Make sure to assign a valid app scheme URL
+            .with(appUrlScheme: appURLScheme)
+            // Video quality for video recording (and sometimes exporting.)
+            .with(videoQuality: .typeHigh)
+            // Starting in iOS 11, you can export images in HEIF or JPEG by setting this value to
+            // `.current` or `.compatible` respectively.
+            // Here we state we prefer HEIF for image export.
+            .with(imageURLExportPreset: .current)
+            // Starting in iOS 11, you can decide what format and quality will be used for exported videos.
+            // Here we state we want to export HEVC at the highest quality.
+            .with(videoExportPreset: AVAssetExportPresetHEVCHighestQuality)
+            // Allow up to 10 files to be picked.
+            .with(maximumSelectionLimit: 10)
+            // Enable image editor for files picked from the photo library.
+            .withEditorEnabled()
+            // Enable a list of cloud sources.
+            .with(availableCloudSources: [.dropbox, .googleDrive, .googlePhotos, .customSource])
+            // Enable a list of local sources.
+            .with(availableLocalSources: [.camera, .photoLibrary, .documents])
+            // Specify what UTIs are allowed for documents picked from Apple's Document Picker (aka iOS Files.)
+            .with(documentPickerAllowedUTIs: ["public.item"])
+            // Specify what UTIs are allowed for files picked from cloud providers.
+            .with(cloudSourceAllowedUTIs: ["public.item"])
+            .build()
+
+        // Instantiate the Filestack `Client` by passing an API key obtained from https://dev.filestack.com/,
+        // together with a `Security` and `Config` object.
+        // If your account does not have security enabled, then you can omit this parameter or set it to `nil`.
+        client = Filestack.Client(apiKey: filestackAPIKey, security: security, config: config)
     }
 }
--- Demo/FilestackDemo/ViewController.swift
+++ Demo/FilestackDemo/ViewController.swift
@@ -6,15 +6,9 @@
 //  Copyright © 2017 Filestack. All rights reserved.
 //
 
-import AVFoundation.AVAssetExportSession
 import Filestack
 import FilestackSDK
 import UIKit
-
-// Set your Filestack's API key here.
-private let filestackAPIKey = "YOUR-FILESTACK-API-KEY"
-// Set your Filestack's app secret here.
-private let filestackAppSecret = "YOUR-FILESTACK-APP-SECRET"
 
 class ViewController: UIViewController {
     @IBOutlet var pickerButton: UIButton!
@@ -35,75 +29,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func presentPicker(_: AnyObject) {
-        // In case your Filestack account has security enabled, you will need to instantiate a `Security` object.
-        // We can do this by either configuring a `Policy` and instantiating a `Security` object by passing
-        // the `Policy` and an `appSecret`, or by instantiating a `Security` object directly by passing an already
-        // encoded policy together with its corresponding signature — in this example, we will use the 1st method.
-
-        // Create `Policy` object with an expiry time and call permissions.
-        let policy = Policy(expiry: .distantFuture,
-                            call: [.pick, .read, .stat, .write, .writeURL, .store, .convert, .remove, .exif])
-
-        // Create `Security` object based on our previously created `Policy` object and app secret obtained from
-        // [Filestack Developer Portal](https://dev.filestack.com/).
-        guard let security = try? Security(policy: policy, appSecret: filestackAppSecret) else {
-            fatalError("Unable to instantiate Security object.")
-        }
-
-        // Create `Config` object.
-        let config = Filestack.Config()
-
-        // IMPORTANT: - Make sure to assign an app scheme URL that matches the one(s) configured in your info.plist.
-        // In our demo this is "filestackdemo".
-        config.appURLScheme = appURLScheme
-
-        // Video quality for video recording (and sometimes exporting.)
-        config.videoQuality = .typeHigh
-
-        if #available(iOS 11.0, *) {
-            // On iOS 11, you can export images in HEIF or JPEG by setting this value to `.current` or `.compatible`
-            // respectively.
-            // Here we state we prefer HEIF for image export.
-            config.imageURLExportPreset = .current
-            // On iOS 11, you can decide what format and quality will be used for exported videos.
-            // Here we state we want to export HEVC at the highest quality.
-            config.videoExportPreset = AVAssetExportPresetHEVCHighestQuality
-        }
-
-        // Here you can enumerate the available local sources for the picker.
-        // If you simply want to enumerate all the local sources, you may use `LocalSource.all()`, but if you would
-        // like to enumerate, let's say the camera source only, you could set it like this:
-        //
-        //   config.availableLocalSources = [.camera]
-        //
-        config.availableLocalSources = LocalSource.all()
-
-        // Here you can enumerate the available cloud sources for the picker.
-        // If you simply want to enumerate all the cloud sources, you may use `CloudSource.all()`, but if you would
-        // like to enumerate selected cloud sources, you could set these like this:
-        //
-        //   config.availableCloudSources = [.dropbox, .googledrive, .googlephotos, .customSource]
-        //
-        config.availableCloudSources = CloudSource.all()
-
-        // Here you can decide what document types can be picked when using Apple's document picker.
-        config.documentPickerAllowedUTIs = ["public.item"]
-        // In the same fashion, you can decide what document types can be picked from cloud sources.
-        config.cloudSourceAllowedUTIs = ["public.item"]
-        //
-        // In both cases we have used ["public.item"], which means we allow picking any files or bundles.
-        // For instance, if you wanted to *only* allow picking images, videos, and audios, you would instead use:
-        //
-        //   config.documentPickerAllowedUTIs = ["public.image", "public.video", "public.audio"]
-        //   config.cloudSourceAllowedUTIs = ["public.image", "public.video", "public.audio"]
-        //
-        // For a comprehensive list of UTIs, please consult:
-        //   https://developer.apple.com/library/content/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html#//apple_ref/doc/uid/TP40009259-SW1
-
-        // Instantiate the Filestack `Client` by passing an API key obtained from [Filestack Developer Portal](https://dev.filestack.com/),
-        // together with a `Security` and `Config` object.
-        // If your account does not have security enabled, then you can omit this parameter or set it to `nil`.
-        let client = Filestack.Client(apiKey: filestackAPIKey, security: security, config: config)
+        guard let client = client else { return }
 
         // Store options for your uploaded files.
         // Here we are saying our storage location is S3 and access for uploaded files should be public.
