0f2c8d6a52cc742a1b43d2fa0efcbf563381a0a7
Ruben Nine <ruben@9labs.io>
2019-09-12 16:29:57 +0200

    Rendering image in editor using a CGImage-backed UIImage for compatibility with iOS 13.

--- Filestack/UI/Internal/PhotoEditor/EditionController/Views/ImageEditorView.swift
+++ Filestack/UI/Internal/PhotoEditor/EditionController/Views/ImageEditorView.swift
@@ -11,12 +11,17 @@ import UIKit
 class ImageEditorView: UIView {
     weak var image: UIImage? {
         didSet {
+            cgBackedImage = image?.cgImageBackedCopy()
             setNeedsDisplay()
         }
     }
 
+    // CIImage-backed UIImages are not rendered correctly in iOS 13 if they have some transformations
+    // applied (e.g. cropping), so we use a CGImage-backed UIImage instead for rendering.
+    private var cgBackedImage: UIImage?
+
     override func draw(_: CGRect) {
-        guard let image = image else { return }
+        guard let image = cgBackedImage else { return }
 
         // Clear background
         UIColor.clear.setFill()
@@ -25,7 +30,7 @@ class ImageEditorView: UIView {
         // Calculate image draw rect
         let imageRect = CGRect(origin: CGPoint.zero, size: image.size)
         let ratio = max(imageRect.width / bounds.width, imageRect.height / bounds.height)
-        let size = CGSize(width: (imageRect.size.width / ratio).rounded(), height: (imageRect.size.height / ratio).rounded())
+        let size = CGSize(width: (imageRect.width / ratio).rounded(), height: (imageRect.height / ratio).rounded())
         let origin = CGPoint(x: (bounds.midX - (size.width / 2)).rounded(), y: (bounds.midY - (size.height / 2)).rounded())
         let drawRect = CGRect(origin: origin, size: size)
 
