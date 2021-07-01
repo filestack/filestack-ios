2aa9b78d5601fb0a156d7d165c89dd88fb285e84
Ruben Nine <ruben@9labs.io>
2021-07-01 09:12:17 +0200

    Leaving library type unspecified. Adding `Sources/Filestack/VERSION` symlink to `VERSION`.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Filestack",
-            type: .dynamic,
             targets: ["Filestack"]
         ),
     ],
