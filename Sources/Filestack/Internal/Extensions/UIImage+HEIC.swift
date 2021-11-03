010c8c03f46059abe69991bdf6a7aff3be8d8e2a
Ruben Nine <ruben@9labs.io>
2021-11-03 10:32:04 +0100

    Fix wrong image orientation on exported HEIC images.

--- Sources/Filestack/Internal/Extensions/UIImage+HEIC.swift
+++ Sources/Filestack/Internal/Extensions/UIImage+HEIC.swift
@@ -14,17 +14,44 @@ extension UIImage {
         var imageData: Data?
         let destinationData = NSMutableData()
 
-        if let destination = CGImageDestinationCreateWithData(destinationData, AVFileType.heic as CFString, 1, nil),
-            let cgImage = cgImage {
-            let options = [kCGImageDestinationLossyCompressionQuality: quality]
-            CGImageDestinationAddImage(destination, cgImage, options as CFDictionary)
-            CGImageDestinationFinalize(destination)
-
-            imageData = destinationData as Data
-
-            return imageData
+        guard
+            let cgImage = cgImage,
+            let destination = CGImageDestinationCreateWithData(destinationData, AVFileType.heic as CFString, 1, nil)
+        else {
+            return nil
         }
 
-        return nil
+        let options: [CFString: Any] = [
+            kCGImageDestinationLossyCompressionQuality: quality,
+            kCGImagePropertyOrientation: cgImageOrientation.rawValue
+        ]
+
+        CGImageDestinationAddImage(destination, cgImage, options as CFDictionary)
+        CGImageDestinationFinalize(destination)
+
+        imageData = destinationData as Data
+
+        return imageData
+    }
+}
+
+extension UIImage {
+    var cgImageOrientation: CGImagePropertyOrientation { .init(imageOrientation) }
+}
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+        @unknown default:
+            fatalError()
+        }
     }
 }
